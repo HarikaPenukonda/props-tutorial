2571e435c5e08a7eeb8886ec28811d1e4dc9e165
HarikaPenukonda <penukondaharika@gmail.com>
2023-07-11 11:59:33 -0500

    adding firstname and lastname

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import React from "react";
 import ReactDOM  from "react-dom";
 
 function App (){
+    const firstName = "Joe"
+    const lastName = "Alwyn"
+    /*
+        Challenge: finish off the h1 below so it says
+        "Hello Joe Alwyn"
+    */
     return(
-        <h1>Hello</h1>
+        <h1>Hello {firstName} {lastName}</h1>
     )
 }
 
