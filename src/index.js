824c3846ebbc5576454d12294ae562442cd3c91b
HarikaPenukonda <penukondaharika@gmail.com>
2023-07-11 12:08:02 -0500

    seperating the values date and hours

--- src/index.js
+++ src/index.js
@@ -8,9 +8,10 @@ function App (){
         Challenge: finish off the h1 below so it says
         "Hello Joe Alwyn"
     */
-   //const date = new Date()
+    const date = new Date()
+    const hours = date.getHours()
     return(
-        <h1>It is currently about {new Date().getHours()} o' clock</h1>
+        <h1>It is currently about {hours} o' clock</h1>
     )
 }
 
