8b6410e576ec3787020c9ddb2c7bc119a7df5edd
HarikaPenukonda <penukondaharika@gmail.com>
2023-07-11 13:45:40 -0500

    creating prop values

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react"
 import ContactCard from "./props/ContactCard"
 
 /*
-    
+    Challenge: Add all the rest of the data to the Contact card instances
 */
 
 export default function App (){
@@ -11,10 +11,27 @@ export default function App (){
             <ContactCard 
                 img="./images/cat1.jpeg"
                 name="Mr. Whiskerson"
+                phone="(212) 555-1234"
+                email="mr.whiskaz@catnap.meow"
                 />
-            <ContactCard/>
-            <ContactCard/>
-            <ContactCard/>
+            <ContactCard
+                img="./images/cat2.jpeg"
+                name="Fluffykins"
+                phone="(212) 555-2345"
+                email="fluff@me.com"
+            />
+            <ContactCard
+                img="./images/cat3.jpeg"
+                name="Felix"
+                phone="(212) 555-4567"
+                email="thecat@hotmail.com"
+            />
+            <ContactCard
+                img="./images/cat4.jpeg"
+                name="Pumpkin"
+                phone="(212) 555-1234"
+                email="pumpkin@gmail.com"
+            />
         </div>
         
 
